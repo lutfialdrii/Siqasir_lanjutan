3ed68d54f587adf842dd32d82648a23e1f204eb8
lutfialdrii <12050110415@students.uin-suska.ac.id>
2022-03-14 07:05:44 +0700

    cek

--- app/src/main/java/com/example/siqasir_lanjutan/activity/LoginActivity.java
+++ app/src/main/java/com/example/siqasir_lanjutan/activity/LoginActivity.java
@@ -2,47 +2,43 @@ package com.example.siqasir_lanjutan.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.siqasir_lanjutan.R;
 
-import java.util.Locale;
-
 public class LoginActivity extends AppCompatActivity {
 
-    Button login;
+    ImageButton login;
     EditText username, password;
 
-    protected void onCreate (Bundle saveInstanceState){
+    protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
 
         setContentView(R.layout.login_relative);
 
-        login = (Button) findViewById(R.id.btnLogin);
+        login = (ImageButton) findViewById(R.id.btnLogin);
         username = (EditText) findViewById(R.id.etUsername);
         password = (EditText) findViewById(R.id.etPassword);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (password.getText().toString().toUpperCase().equals("1234")){
-
-                } else {
-                    Toast.makeText(getApplicationContext(),"Password Salah!", Toast.LENGTH_SHORT).show();
-                }
+        login.setOnClickListener(view -> {
+            if (password.getText().toString().equalsIgnoreCase("1234")) {
+                login_sukses();
+            } else {
+                Toast.makeText(getApplicationContext(), "salah Password", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void login_sukses(){
+
+    private void login_sukses() {
         String usernameLogin = username.getText().toString();
-        Intent intent = new Intent(this,MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         //mengirim data ke tampilan utama
-        intent.putExtra("USERNAME",usernameLogin);
+        intent.putExtra("USERNAME", usernameLogin);
         startActivity(intent);
     }
 }
+
